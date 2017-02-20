d601dbdac0682ce023a9a247c1de96ffc64d5210
Roman Rozhkov <rrozhkov@luxoft.com>
2017-02-20 11:44:40 +0600

    Refactoring

--- src/main/java/ru/rrozhkov/lib/db/IDBManager.java
+++ src/main/java/ru/rrozhkov/lib/db/IDBManager.java
@@ -9,17 +9,17 @@ import ru.rrozhkov.lib.convert.IConverter;
 
 public interface IDBManager {
 
-	public abstract int nextId(String tableName) throws SQLException;
+	int nextId(String tableName) throws SQLException;
 
-	public abstract <T> Collection<T> select(String select,
+	<T> Collection<T> select(String select,
 			IConverter<ResultSet, T> converter) throws SQLException;
 	
 	int insert(String sql, Map<String, Object> map)  throws SQLException;
 	
 	int update(String sql, Map<String, Object> map)  throws SQLException;
 
-	public abstract ResultSet executeQuery(String query) throws SQLException;
+	ResultSet executeQuery(String query) throws SQLException;
 
-	public abstract int executeUpdate(String query) throws SQLException;
+	int executeUpdate(String query) throws SQLException;
 
 }
